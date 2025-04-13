e3e4d2c5fbab338716ac4356306686d1cf5d8b8a
yosra <gadhgadhiyosra2@gmail.com>
2025-04-13 18:43:37 +0100

    feature: Create Supplier

---
+++ controllers/SupplierController.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import models.Supplier;
+import services.SupplierService;
+
+import java.util.ArrayList;
+
+public class SupplierController {
+    private static SupplierController instance;
+    private final SupplierService supplierService;
+
+    private SupplierController() {
+        if (instance == null) {
+            instance = this;
+        }
+        this.supplierService = SupplierService.getInstance();
+    }
+
+    public static SupplierController getInstance() {
+        if (instance == null) {
+            instance = new SupplierController();
+        }
+        return instance;
+    }
+
+    public void addSupplier(String name, String phone, String address) {
+        supplierService.addSupplier(name, phone, address);
+    }
+
+    public void updateSupplier(Supplier supplier) {
+        supplierService.updateSupplier(supplier);
+    }
+
+    public void deleteSupplier(int id) {
+        supplierService.deleteSupplier(id);
+    }
+
+    public Supplier getSupplierById(int id) {
+        return supplierService.getSupplierById(id);
+    }
+
+    public ArrayList<Supplier> listCategories() {
+        return supplierService.listSuppliers();
+    }
+}
---
+++ repositories/SupplierRepository.java
@@ -0,0 +1,98 @@
+package repositories;
+
+import database.DBConnection;
+import models.Admin;
+import models.Supplier;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SupplierRepository {
+    public static SupplierRepository instance ;
+    public final Connection connection;
+
+
+    private SupplierRepository () {
+        this.connection = DBConnection.getInstance().getConnection();
+
+    }
+    public static SupplierRepository getInstance () {
+        if (instance== null){
+            instance = new SupplierRepository() ;
+        }
+           return instance;
+    }
+
+    public void save(Supplier supplier) throws SQLException {
+
+
+        String sql = "INSERT INTO supplier (id, name, phone,address) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, supplier.getId());
+            stmt.setString(2, supplier.getName());
+            stmt.setString(3, supplier.getPhone());
+            stmt.setString(3, supplier.getAddress());
+            stmt.executeUpdate();
+        }
+    }
+
+    public Supplier getById(int id) throws SQLException {
+        String sql = "SELECT * FROM supplier WHERE id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new Supplier(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("phone"),
+                        rs.getString("address")
+                );
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Supplier> getAll() throws SQLException {
+        ArrayList<Supplier> suppliers = new ArrayList<>();
+        String sql = "SELECT * FROM supplier ";
+        try (Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                suppliers.add(new Supplier(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("phone"),
+                       rs.getString("address")
+
+                ));
+            }
+        }
+        return suppliers;
+    }
+
+    public void delete(int id) throws SQLException {
+        String sql = "DELETE FROM supplier WHERE id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        }
+    }
+
+    public void update(Supplier existingSupplier) throws SQLException {
+        String sql = "UPDATE supplier SET name = ?, phone = ?, address = ? WHERE id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, existingSupplier.getName());
+            stmt.setString(2, existingSupplier.getPhone());
+            stmt.setString(3, existingSupplier.getAddress());
+            stmt.setInt(4, existingSupplier.getId());
+            stmt.executeUpdate();
+        }
+    }
+}
+
+
+
+
