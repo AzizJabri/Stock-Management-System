25b81b67b8208715705fd703a7cf1c606b0b77ce
yosra <gadhgadhiyosra2@gmail.com>
2025-04-20 20:27:42 +0100

    feature: main OrderController

--- controllers/OrderController.java
+++ controllers/OrderController.java
@@ -1,15 +1,19 @@
 package controllers;
 
 import models.Order;
+import models.Supplier;
 import services.OrderService;
 import services.ProductService;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
 
 public class OrderController {
     private static OrderController instance;
     private final OrderService orderService;
+
+    private final Scanner scanner = new Scanner(System.in);
 
     private OrderController() {
 
@@ -23,21 +27,73 @@ public class OrderController {
         return instance;
     }
 
-    public void addOrder(int customerId, String status, double totalAmount) {
-        Date now = new Date();
-        orderService.addOrder(customerId, now, status, totalAmount);
+    public void addOrder() {
+        try {
+            System.out.println("Entrer Order customerId: ");
+            int customerId = Integer.parseInt(scanner.nextLine());
+            System.out.println("Entrer Order status: ");
+            String status = scanner.nextLine();
+            System.out.println("Entrer Order totalAmount: ");
+            double totalAmount = Double.parseDouble(scanner.nextLine());
+
+
+            Date now = new Date();
+            orderService.addOrder(customerId, now, status, totalAmount);
+
+
+        } catch (Exception e) {
+            System.out.println("Error while a adding product: " + e.getMessage());
+
+        }
+
     }
 
-    public void updateOrder(Order order) {
+    public void updateOrder() {
+        System.out.println("Enter Order ID to update :");
+        int id = Integer.parseInt(scanner.nextLine());
+        Order order = orderService.getOrderById(id);
+        if (order== null){
+            System.out.println(
+                    "Order not found"
+            );
+            return;
+        }
+        System.out.println("Entrer order customerId: ");
+        int customerId = Integer.parseInt(scanner.nextLine());
+        System.out.println("Entrer Order status: ");
+        String status = scanner.nextLine();
+        System.out.println("Entrer Order totalAmount: ");
+        double totalAmount = Double.parseDouble(scanner.nextLine());
+
         orderService.updateOrder(order);
     }
 
-    public void deleteOrder(int orderId) {
-        orderService.deleteOrder(orderId);
+    public void deleteOrder() {
+        System.out.println("Enter order ID to delete :");
+        int id = Integer.parseInt(scanner.nextLine());
+        Order order = orderService.getOrderById(id);
+        if (order== null){
+            System.out.println(
+                    "Order not found"
+            );
+            return;
+        }
+
+        orderService.deleteOrder(id);
     }
 
-    public Order getOrderById(int orderId) {
-        return orderService.getOrderById(orderId);
+    public Order getOrderById() {
+        System.out.println("Enter order ID to delete :");
+        int id = Integer.parseInt(scanner.nextLine());
+        Order order = orderService.getOrderById(id);
+        if (order== null){
+            System.out.println(
+                    "Order not found"
+            );
+            return null;
+        }
+
+        return orderService.getOrderById(id);
     }
 
     public ArrayList<Order> listAllOrders() {
