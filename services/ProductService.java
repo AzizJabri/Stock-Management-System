b6a396033163c760306c8cbfced2e86165dc5d3d
AzizJabri <azizjb.business@gmail.com>
2025-04-20 17:35:11 +0100

    Fix: Fixed products management

--- services/ProductService.java
+++ services/ProductService.java
@@ -2,16 +2,23 @@ package services;
 
 import interfaces.IProductService;
 import models.Product;
+import repositories.CategoryRepository;
 import repositories.ProductRepository;
+import repositories.SupplierRepository;
 
 import java.util.ArrayList;
 
 public class ProductService implements IProductService {
     private static ProductService instance;
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    private final SupplierRepository supplierRepository;
 
     private ProductService() {
         this.productRepository = ProductRepository.getInstance();
+        this.categoryRepository = CategoryRepository.getInstance();
+        this.supplierRepository = SupplierRepository.getInstance();
     }
 
     public static ProductService getInstance() {
@@ -25,6 +32,15 @@ public class ProductService implements IProductService {
                            int quantity_in_stock, int supplier_id, java.util.Date created_at,
                            java.util.Date updated_at) {
         try {
+            // Check if category and supplier exist
+            if (categoryRepository.getById(category_id) == null) {
+                System.out.println("Category not found");
+                return;
+            }
+            if (supplierRepository.getById(supplier_id) == null) {
+                System.out.println("Supplier not found");
+                return;
+            }
             Product product = new Product(category_id, name, description, price,
                     quantity_in_stock, supplier_id, created_at, updated_at);
             productRepository.save(product);
@@ -35,6 +51,15 @@ public class ProductService implements IProductService {
 
     public void updateProduct(Product product) {
         try {
+            // Check if category and supplier exist
+            if (categoryRepository.getById(product.getCategory_id()) == null) {
+                System.out.println("Category not found");
+                return;
+            }
+            if (supplierRepository.getById(product.getSupplier_id()) == null) {
+                System.out.println("Supplier not found");
+                return;
+            }
             productRepository.update(product);
         } catch (Exception e) {
             System.out.println("Error updating Product: " + e.getMessage());
